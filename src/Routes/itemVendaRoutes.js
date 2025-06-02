77db10428f3bd2c617fbbf4bca6c7762497bfd71
PedroR2401 <phromao@outlook.com>
2025-06-02 15:41:58 -0300

    Ajustes Finais
    Ajustado nome de funcoes e token funcionando nos endpoints

--- src/Routes/itemVendaRoutes.js
+++ src/Routes/itemVendaRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const itemVendaController = require('../controller/item_vendaController');
 const token = require('../middleware/token');
 
-router.get('/', itemVendaController.listarItemVenda);
+router.get('/', itemVendaController.listarItensVenda);
 router.post('/novo', token, itemVendaController.adicionarItemVenda);
-router.get('/:id', itemVendaController.editarItemVenda);
+router.get('/:id', itemVendaController.buscarItemVenda);
 router.put('/:id', token, itemVendaController.alterarItemVenda);
 router.delete('/excluir/:id', token, itemVendaController.excluirItemVenda);
 
