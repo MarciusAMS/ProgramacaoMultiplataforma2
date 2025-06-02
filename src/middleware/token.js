5cee46c69fa5afc510924b9892c439138c5288cc
Marcius Vinicius Esquecula <marciusesquecula@gmail.com>
2025-06-01 22:16:02 -0300

    🔨 Token ajustado.

--- src/middleware/token.js
+++ src/middleware/token.js
@@ -2,17 +2,20 @@ const jwt = require('jsonwebtoken');
 const { JWT_KEY } = require('../utils/utils');
 
 module.exports = (req, res, next) => {
-    try {
-        // se usar bearer: bearer <token>
-        //const token = req.headers.authorization.split(" ")[1];
-        // se não usar bearer:
-        const token = req.headers.authorization;
-        const decoded = jwt.verify(token, JWT_KEY);
-        req.userData = decoded;
-        next();
-    } catch (error) {
-        return res.status(401).json({
-            mensagem: 'Falha na autenticação'
-        });
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ mensagem: 'Token não informado!' });
     }
-}
+
+    const token = authHeader.split(' ')[1]; // Bearer TOKEN
+
+    jwt.verify(token, JWT_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ mensagem: 'Token inválido!' });
+        }
+
+        req.user = decoded; // Aqui vai ter o email
+        next();
+    });
+};
